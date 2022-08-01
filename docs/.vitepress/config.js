28812450e2dd49ff06bfb2c652e2b301d27de323
wehuss <1245613916@qq.com>
2022-08-01 16:17:31 +0800

    config🔧:  修改文档地址

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -3,7 +3,7 @@ import { defineConfig } from "vitepress";
 export default defineConfig({
   title:'Arco Vue Pro Components',
   description:'基于arco-design-vue二次封装的组件库',
-  base:'/arco-v-pro-components/',
+  base:'/arco-vue-pro-components/',
   themeConfig:{
     logo:'logo.svg'
   }
