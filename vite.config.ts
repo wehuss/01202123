290224e3a7a3044aa4d5eb785787626147e08e38
wehuss <1245613916@qq.com>
2022-08-02 17:12:17 +0800

    config🔧:  加入插件

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+// import { createStyleImportPlugin } from 'vite-plugin-style-import'
+import vitePluginForArco from '@arco-plugins/vite-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [vue(),
+    vueJsx(),
+    vitePluginForArco({
+      style: true,
+    }),
+  ],
 })
